c6815d856c99b9a2bceb680e3a3e1e67f3bf3519
Doan Viet Hoang <38163606+hoangdv99@users.noreply.github.com>
2024-08-13 19:21:08 +0900

    Time: 1 ms (73.53%) | Memory: 2.2 MB (94.22%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.go
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.go
@@ -1,7 +1,4 @@
 func strStr(haystack string, needle string) int {
-    if haystack == needle {
-        return 0
-    }
 	for i := range haystack {
 		if i+len(needle) <= len(haystack) && haystack[i:i+len(needle)] == needle {
 			return i

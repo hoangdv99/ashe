4deeaf25308825ff8bfe0980ac831a658331cc24
Doan Viet Hoang <38163606+hoangdv99@users.noreply.github.com>
2024-08-14 09:57:43 +0900

    Time: 3 ms (70.17%) | Memory: 3.1 MB (73.16%) - LeetSync

--- 35-search-insert-position/search-insert-position.go
+++ 35-search-insert-position/search-insert-position.go
@@ -1,8 +1,25 @@
+// func searchInsert(nums []int, target int) int {
+// 	for i, e := range nums {
+// 		if e >= target {
+// 			return i
+// 		}
+// 	}
+// 	return len(nums)
+// }
+
 func searchInsert(nums []int, target int) int {
-	for i, e := range nums {
-		if e >= target {
-			return i
-		}
-	}
-	return len(nums)
+    left, right := 0, len(nums)-1
+
+    for left <= right {
+        mid := left + (right-left)/2
+        if nums[mid] == target {
+            return mid
+        } else if nums[mid] < target {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+
+    return left
 }

5019f19c399a448a3c144b9a926bf59d8f332a43
Doan Viet Hoang <38163606+hoangdv99@users.noreply.github.com>
2024-10-29 15:39:36 +0900

    Time: 0 ms (100.00%) | Memory: 9.9 MB (23.70%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.go
@@ -0,0 +1,12 @@
+func maxProfit(prices []int) int {
+    profit := 0
+	minPrice := prices[0]
+	for i := 0; i < len(prices); i++ {
+		if prices[i] < minPrice {
+			minPrice = prices[i]
+		} else if prices[i] > minPrice && prices[i]-minPrice > profit {
+			profit = prices[i] - minPrice
+		}
+	}
+	return profit
+}

c042b40eec6c44b87d8bd15836d7a0e57af82abb
Doan Viet Hoang <38163606+hoangdv99@users.noreply.github.com>
2024-10-31 18:14:43 +0900

    Time: 0 ms (100.00%) | Memory: 8.2 MB (15.54%) - LeetSync

--- 169-majority-element/majority-element.go
+++ 169-majority-element/majority-element.go
@@ -1,15 +1,31 @@
+// func majorityElement(nums []int) int {
+//     m := make(map[int]int)
+//     for _, num := range nums {
+//         _, ok := m[num]
+//         if !ok {
+//             m[num] = 1
+//         } else {
+//             m[num] += 1
+//         }
+//         if m[num] > len(nums) / 2 {
+//             return num
+//         } 
+//     }
+//     return 0
+// }
+
 func majorityElement(nums []int) int {
-    m := make(map[int]int)
+    count := 0
+    candidate := 0
     for _, num := range nums {
-        _, ok := m[num]
-        if !ok {
-            m[num] = 1
-        } else {
-            m[num] += 1
+        if count == 0 {
+            candidate = num
         }
-        if m[num] > len(nums) / 2 {
-            return num
-        } 
+        if candidate == num {
+            count++
+        } else {
+            count--
+        }
     }
-    return 0
+    return candidate
 }

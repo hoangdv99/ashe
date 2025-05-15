d7244dc0a105d4ec377bf06ee21329fe37de9440
Doan Viet Hoang <38163606+hoangdv99@users.noreply.github.com>
2025-05-15 17:58:18 +0900

    Time: 3 ms (56.81%) | Memory: 6.5 MB (54.61%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.go
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.go
@@ -1,18 +1,38 @@
+// func reverseVowels(s string) string {
+//     runes := []rune(s)
+//     vowels := []rune{'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'}
+//     vowelsInS := []rune{}
+//     for _, c := range s {
+//         if slices.Contains(vowels, c) {
+//             vowelsInS = append(vowelsInS, c)
+//         }
+//     }
+//     j := len(vowelsInS)-1
+//     for i, c := range s {
+//         if slices.Contains(vowels, c) {
+//             runes[i] = vowelsInS[j]
+//             j--
+//         }
+//     }
+//     return string(runes)
+// }
+
 func reverseVowels(s string) string {
+    i := 0
+    j := len(s) - 1
     runes := []rune(s)
     vowels := []rune{'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'}
-    vowelsInS := []rune{}
-    for _, c := range s {
-        if slices.Contains(vowels, c) {
-            vowelsInS = append(vowelsInS, c)
-        }
-    }
-    j := len(vowelsInS)-1
-    for i, c := range s {
-        if slices.Contains(vowels, c) {
-            runes[i] = vowelsInS[j]
+    for i < j {
+        if slices.Contains(vowels, runes[i]) {
+            for !slices.Contains(vowels, runes[j]) {
+                j--
+            }
+            tmp := runes[i]
+            runes[i] = runes[j]
+            runes[j] = tmp
             j--
         }
+        i++
     }
     return string(runes)
 }
